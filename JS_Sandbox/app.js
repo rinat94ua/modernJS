83f0753f75cc1f1293232bdc3ebca34027fe0948
vodo4ka <rinat94ua@gmail.com>
2018-04-02 23:39:02 +0300

    first commit

---
+++ JS_Sandbox/app.js
@@ -0,0 +1,17 @@
+//var, let, const
+
+// var name = 'John Doe';
+// console.log(name);
+
+// // multi word vars
+// var firstName = 'John'; //Camel case
+
+//LET
+let name;
+name = "John Doe";
+
+// const
+const name = "John";
+console.log(name);
+// name = "asdasd";
+// console.log(name);
