b80329a8b884d426d66a123fb4f65bd9ee64270c
vodo4ka <rinat94ua@gmail.com>
2018-04-10 21:40:45 +0300

    chucknorrisJokes

---
+++ AJAX/XHR_JSON/app.js
@@ -0,0 +1,56 @@
+document.getElementById('button1').addEventListener('click', loadCustomer);
+document.getElementById('button2').addEventListener('click', loadCustomers);
+
+function loadCustomer(e) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.open('GET', 'customer.json', true);
+
+  xhr.onload = function () {
+    if(this.status === 200) {
+      // console.log(this.responseText);
+
+      const customer = JSON.parse(this.responseText);
+
+      const output = `
+        <ul>
+          <li>ID: ${customer.id}</li>
+          <li>name: ${customer.name}</li>
+          <li>company: ${customer.company}</li>
+          <li>phone: ${customer.phone}</li>
+        </ul>
+      `;
+
+      document.getElementById('customer').innerHTML = output;
+    }
+  }
+
+  xhr.send()
+}
+
+function loadCustomers(e){
+  const xhr = new XMLHttpRequest();
+
+  xhr.open('GET', 'customers.json', true);
+
+  xhr.onload = function(){
+    if (this.status === 200) {
+      const customers = JSON.parse(this.responseText);
+      let output = '';
+
+      customers.forEach(function(customer){
+        output += `
+          <ul>
+            <li>ID: ${customer.id}</li>
+            <li>name: ${customer.name}</li>
+            <li>company: ${customer.company}</li>
+            <li>phone: ${customer.phone}</li>
+          </ul>
+        `;
+      });
+      document.getElementById('customers').innerHTML = output;
+    }
+  }
+
+  xhr.send();
+}
