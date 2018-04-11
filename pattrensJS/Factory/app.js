494796a8832efc8b2ac9305804a06546d649df25
vodo4ka <rinat94ua@gmail.com>
2018-04-11 15:50:14 +0300

    Observer pattern

---
+++ pattrensJS/Factory/app.js
@@ -0,0 +1,47 @@
+function MemberFactory() {
+  this.createMember = function(name, type) {
+    let member;
+
+    if(type === 'simple'){
+      member = new SimpleMembership(name);
+    } else if(type === 'standard') {
+      member = new StandardMembership(name);
+    } else if (type === 'super') {
+      member = new SuperMembership(name);
+    }
+
+    member.type = type;
+
+    member.define = function () {
+      console.log(`${this.name} (${this.type}): ${this.cost}`);
+    }
+
+    return member;
+  }
+};
+
+const SimpleMembership = function (name) {
+  this.name = name;
+  this.cost = '5$';
+}
+
+const StandardMembership = function (name) {
+  this.name = name;
+  this.cost = '15$';
+}
+const SuperMembership = function (name) {
+  this.name = name;
+  this.cost = '25$';
+}
+
+const members = [];
+const factory = new MemberFactory();
+
+members.push(factory.createMember(`John doe`, 'simple'));
+members.push(factory.createMember(`Joh1`, 'standard'));
+members.push(factory.createMember(`Joh23`, 'super'));
+
+
+members.forEach(function(member){
+  member.define();
+});
